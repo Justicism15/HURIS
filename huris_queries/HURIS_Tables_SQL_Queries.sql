9ce68c3bd21d2269742277ce8a0224e6793804be
[JR] <[destua0824@gmail.com]>
2018-11-16 15:43:24 +0800

    JR

--- huris_queries/HURIS_Tables_SQL_Queries.sql
+++ huris_queries/HURIS_Tables_SQL_Queries.sql
@@ -1,6 +1,59 @@
 use USLS
 go
 
+--CREATE TABLE [dbo].[Sub_Department]
+--(
+--	[sub_department_ID] INT IDENTITY (101,1) NOT NULL,
+--	[sub_department_name] NVARCHAR (100) NOT NULL,
+--	[sub_department_address] NVARCHAR (100) NULL,
+--	[sub_department_desc] NVARCHAR (100) NULL,
+--	--[department_ID] INT FOREIGN KEY REFERENCES Department(department_ID),
+--	CONSTRAINT [PK_Sub_Department] PRIMARY KEY CLUSTERED ([sub_department_ID] ASC)
+--);
+
+
+CREATE TABLE [dbo].[Department]
+(
+	[department_ID] INT IDENTITY(101,1) NOT NULL,
+	[department_name] NVARCHAR (100) NOT NULL,
+	[department_address] NVARCHAR (100) NULL,
+	[department_desc] NVARCHAR (100) NULL, 
+	[sub_department_ID] INT FOREIGN KEY REFERENCES Sub_Department(sub_department_ID),
+	CONSTRAINT [PK_Department] PRIMARY KEY CLUSTERED ([department_ID] ASC) 
+);
+
+CREATE TABLE [dbo].[Job_Title]
+(
+	[job_title_ID] INT IDENTITY (101,1) NOT NULL,
+	[job_description] NVARCHAR (100) NOT NULL,
+	CONSTRAINT [PK_Job_Title] PRIMARY KEY CLUSTERED ([job_title_ID] ASC)
+);
+
+
+CREATE TABLE [dbo].[Employee_Type]
+(
+	[employee_type_ID] INT IDENTITY (101,1) NOT NULL,
+	[employee_type_name] NVARCHAR (100) NOT NULL,
+	CONSTRAINT [PK_Employee_Type] PRIMARY KEY CLUSTERED ([employee_type_ID] ASC)
+
+
+);
+CREATE TABLE [dbo].[Compensation]
+(
+	[compensation_ID] INT IDENTITY (101,1) NOT NULL,
+	[employee_type_ID] INT FOREIGN KEY REFERENCES Employee_Type (employee_type_ID),
+	CONSTRAINT [PK_Compensation] PRIMARY KEY CLUSTERED ([compensation_ID] ASC) 
+);
+
+
+CREATE TABLE [dbo].[Employee_Status]
+(
+	[empstatus_ID] INT IDENTITY (101,1) NOT NULL,
+	[empstatus_name] NVARCHAR (100) NOT NULL, 
+	CONSTRAINT [PK_Employee_Status] PRIMARY KEY CLUSTERED ([empstatus_ID] ASC)
+);
+
+
 CREATE TABLE [dbo].[Employee]
 (
 	[employee_ID] INT IDENTITY(101,1) NOT NULL,
@@ -13,44 +66,14 @@ CREATE TABLE [dbo].[Employee]
 	[email] NVARCHAR (100) NOT NULL,
 	[mobile_number] NVARCHAR (11) NOT NULL,
 	[telephone_number] NVARCHAR (10) NULL,
-	[department_ID] INT FOREIGN KEY REFERENCES Department(department_id),
-	[job_ID] INT FOREIGN KEY REFERENCES Job_Title(job_id),
-	[compensation_ID] INT FOREIGN KEY REFERENCES Compensation(compensation_id),
-	[empstatus_ID]INT FOREIGN KEY REFERENCES Employee_Status(empstatus_id),
+	[department_ID] INT FOREIGN KEY REFERENCES Department(department_ID),
+	[job_title_ID] INT FOREIGN KEY REFERENCES Job_Title(job_title_ID),
+	[compensation_ID] INT FOREIGN KEY REFERENCES Compensation(compensation_ID),
+	[empstatus_ID]INT FOREIGN KEY REFERENCES Employee_Status(empstatus_ID),
 	CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED ([employee_ID] ASC)
 ); 
 
-CREATE TABLE [dbo].[Department]
-(
-	[department_ID] INT IDENTITY(101,1) NOT NULL,
-	[department_name] NVARCHAR (100) NOT NULL,
-	[sub_department_ID] INT FOREIGN KEY REFERENCES Sub_Department(sub_department_ID),
-	CONSTRAINT [PK_Department] PRIMARY KEY CLUSTERED ([department_ID] ASC) 
-);
 
 
-CREATE TABLE [dbo].[Job_Title]
-(
-	[job_title_ID] INT IDENTITY (101,1) NOT NULL,
-	[job_description] NVARCHAR (100) NOT NULL,
-	CONSTRAINT [PK_Job_Title] PRIMARY KEY CLUSTERED ([job_title_ID] ASC)
-);
 
 
-CREATE TABLE [dbo].[Compensation]
-(
-
-);
-
-
-CREATE TABLE [dbo].[Employee_Status]
-(
-	[empstatus_ID] INT IDENTITY (101,1) NOT NULL,
-	[empstatus_name] NVARCHAR (100) NOT NULL, 
-	CONSTRAINT [PK_Employee_Status] PRIMARY KEY CLUSTERED ([empstatus_ID] ASC)
-);
-
-CREATE TABLE [dbo].[Sub_Department]
-(
-	
-);
